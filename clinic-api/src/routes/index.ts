f48271fb53024f7b0dca817eb98d7d41e2ef4d36
Davi Saldanha <davisaldanha.ti@gmail.com>
2026-09-03 16:51:40 -0300

    feat: implementa routes pacientes

--- clinic-api/src/routes/index.ts
+++ clinic-api/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express"
 import { authRoutes } from "./auth.routes"
+import { pacienteRoutes } from "./paciente.routes";
 
 const routes = Router()
 
 routes.use("/auth", authRoutes)
+routes.use("/pacientes", pacienteRoutes)
 
 export { routes }
