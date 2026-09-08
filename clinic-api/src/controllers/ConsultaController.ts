a855b210692f01a0701025d00e4885ca175c6a7e
Davi Saldanha <davisaldanha.ti@gmail.com>
2026-09-08 14:20:33 -0300

    refactor: identação

--- clinic-api/src/controllers/ConsultaController.ts
+++ clinic-api/src/controllers/ConsultaController.ts
@@ -10,39 +10,39 @@ const medicoRepository =AppDataSource.getRepository(Medico)
 const pacienteRepository =AppDataSource.getRepository(Paciente)
 
 export class ConsultaController{
-      // POST /consultas
-      async agendar(req: Request, res: Response){
-            const { medicoID, dataHora, observacoes } = req.body
+   // POST /consultas
+   async agendar(req: Request, res: Response){
+      const { medicoID, dataHora, observacoes } = req.body
 
-            if (!medicoID || !dataHora){
-               return res.status(400).json({erro: 'medicoID e dataHora são obrigatórios'})
-            }
-
-            const paciente = await pacienteRepository.findOne({
-               where: { usuario: { id: req.usuario?.sub}}
-            })
-
-            if(!paciente){
-               return res.status(404).json({erro: 'Paciente não encontrado.'})
-            }
-
-            const medico = await medicoRepository.findOneBy({ id: medicoID })
-
-            if(!medico){
-               return res.status(404).json({erro: 'Medico não encontrado.'})
-            }
-
-            const consulta = consultaRepository.create({
-               paciente,
-               medico,
-               dataHora: new Date(dataHora),
-               observacoes,
-               status: ConsultaStatus.AGENDADA
-            })
-            await consultaRepository.save(consulta)
-
-            return res.status(201).json(consulta)
+      if (!medicoID || !dataHora){
+         return res.status(400).json({erro: 'medicoID e dataHora são obrigatórios'})
       }
+
+      const paciente = await pacienteRepository.findOne({
+         where: { usuario: { id: req.usuario?.sub}}
+      })
+
+      if(!paciente){
+         return res.status(404).json({erro: 'Paciente não encontrado.'})
+      }
+
+      const medico = await medicoRepository.findOneBy({ id: medicoID })
+
+      if(!medico){
+         return res.status(404).json({erro: 'Medico não encontrado.'})
+      }
+
+      const consulta = consultaRepository.create({
+         paciente,
+         medico,
+         dataHora: new Date(dataHora),
+         observacoes,
+         status: ConsultaStatus.AGENDADA
+      })
+      await consultaRepository.save(consulta)
+
+      return res.status(201).json(consulta)
+   }
 
    // GET /consultas
    async listar(req: Request, res: Response) {
