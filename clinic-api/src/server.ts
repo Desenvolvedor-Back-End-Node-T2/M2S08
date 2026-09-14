89be769ad42be7371729a49947f8baa5a9268893
Davi Saldanha <davisaldanha.ti@gmail.com>
2026-09-14 16:50:09 -0300

    feat: adicionada a configuração do Swagger para documentação da API

--- clinic-api/src/server.ts
+++ clinic-api/src/server.ts
@@ -4,12 +4,16 @@ import express from 'express'
 import cors from 'cors'
 import { AppDataSource } from './data-source'
 import { routes } from './routes';
+import { swaggerSpec } from './config/swagger';
+import swaggerUi from 'swagger-ui-express'
 
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(routes)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 const PORT = process.env.PORT || 3333
 
